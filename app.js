b17954c986e1a0342fd9d61fd762951a210bf2c0
Malinda <ilovefroyos@gmail.com>
2018-12-28 01:14:42 -0800

    setup template engine (express-handlebars)

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
+var exphbs = require('express-handlebars');
 
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    helpers: {
+        section: function(name, options){
+            if(!this._sections) this._sections = {};
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }, 
+    extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+app.use(express.static(__dirname + '/assets'));
+
+// Home Page
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.render('index', { title: 'Home' });
 });
 
 app.listen(port, () => {
