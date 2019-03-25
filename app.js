dd6c833eebfbbd05b3d016e0980eded18aea3b77
Malinda <ilovefroyos@gmail.com>
2019-03-24 22:51:00 -0700

    completed homepage and made small changes for demo
    rneed to refactor code, implement Sass, update Node/Express routing

--- app.js
+++ app.js
@@ -17,11 +17,9 @@ app.engine('.hbs', exphbs({
 app.set('view engine', '.hbs');
 
 app.use(express.static(__dirname + '/assets'));
-// app.use('/', express.static(__dirname + '/assets'));
 
-// Home Page
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Home' });
+    res.render('index', { title: "Citizen's Coolkit" });
 });
 
 app.get('/step-1', (req, res) => {
@@ -29,15 +27,15 @@ app.get('/step-1', (req, res) => {
 });
 
 app.get('/s1a1', (req, res) => {
-    res.render('step-1-activity-1', { title: 'Collecting Stories' });
-});
-
-app.get('/s2a1', (req, res) => {
-    res.render('step-2-activity-1', { title: 'Collecting Stories' });
+    res.render('step-1-activity-1', { title: 'Collecting Stories | Coolkit' });
 });
 
 app.get('/step-2', (req, res) => {
     res.render('step-2-activity-homepage', { title: 'Step 2: Map Your Block' });
+});
+
+app.get('/s2a1', (req, res) => {
+    res.render('step-2-activity-1', { title: 'Urban Forest Quest | Coolkit' });
 });
 
 app.get('/step-3', (req, res) => {
