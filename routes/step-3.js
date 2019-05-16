5cb7f2de5ee938475132d45fe1aa4db59a64d5b1
tiffanywu0313 <hsinyunwu0313@gmail.com>
2019-05-16 16:48:29 -0700

    added 2 pages

--- routes/step-3.js
+++ routes/step-3.js
@@ -10,8 +10,16 @@ router.get('/a1', (req, res) => {
     res.render('step-3-activity-1', { title: 'Rate Your Household | Coolkit' });
 });
 
+router.get('/a1-p2', (req, res) => {
+    res.render('step-3-activity-1-p2', { title: 'Rate Your Household | Coolkit' });
+});
+
 router.get('/a2', (req, res) => {
     res.render('step-3-activity-2', { title: 'Rate Your Block | Coolkit' });
+});
+
+router.get('/a2-p2', (req, res) => {
+    res.render('step-3-activity-2-p2', { title: 'Rate Your Block | Coolkit' });
 });
 
 module.exports = router;
