708f57a8aa4d97ab8ed61a853b4a307d9dfcf1a4
tiffanywu0313 <hsinyunwu0313@gmail.com>
2019-05-16 15:02:42 -0700

    added rating activities

--- routes/step-3.js
+++ routes/step-3.js
@@ -6,4 +6,12 @@ router.get('/', (req, res) => {
     res.render('step-3-activity-homepage', { title: 'Step 3: Rate Your Block' });
 });
 
+router.get('/a1', (req, res) => {
+    res.render('step-3-activity-1', { title: 'Rate Your Household | Coolkit' });
+});
+
+router.get('/a2', (req, res) => {
+    res.render('step-3-activity-2', { title: 'Rate Your Block | Coolkit' });
+});
+
 module.exports = router;
