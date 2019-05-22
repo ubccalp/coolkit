d08afae92ce9bdd4223ae420da4c8fbbf3762674
tiffanywu0313 <hsinyunwu0313@gmail.com>
2019-05-22 10:24:22 -0700

    added new links and pages

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,14 @@ var step_2 = require('./step-2');
 var step_3 = require('./step-3');
 var step_4 = require('./step-4');
 var step_5 = require('./step-5');
+var ed = require('./ed');
 
 router.use('/step-1', step_1);
 router.use('/step-2', step_2);
 router.use('/step-3', step_3);
 router.use('/step-4', step_4);
 router.use('/step-5', step_5);
+router.use('/ed', ed);
 
 router.get('/', (req, res) => {
     res.render('index', { title: "Citizen's Coolkit" });
