b92ddd2617a2f426ca032b1cdacd40b53ad8b194
tiffanywu0313 <hsinyunwu0313@gmail.com>
2019-06-13 11:11:31 -0700

    new pic

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
+var mongo = require('mongodb');
 var express = require('express');
 var exphbs = require('express-handlebars');
 var path = require('path');
 var routes = require('./routes');
+
+
+
+const MongoClient = require('mongodb').MongoClient;
+
+// replace the uri string with your connection string.
+const uri = "mongodb+srv://Coolkit:jDxgrNDb6VLs5sq7@coolkitcluster-pkuje.azure.mongodb.net/test?retryWrites=true&w=majority"
+MongoClient.connect(uri, function(err, client) {
+   if(err) {
+        console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+   }
+   console.log('Connected...');
+   const collection = client.db("test").collection("devices");
+   // perform actions on the collection object
+   client.close();
+});
+
+
+
 
 var app = express();
 
