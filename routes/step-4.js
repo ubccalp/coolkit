dfaaa68d5dc867213c03bc1530b81afd35741f27
tiffanywu0313 <hsinyunwu0313@gmail.com>
2019-06-20 09:43:42 -0700

    activity 4

--- routes/step-4.js
+++ routes/step-4.js
@@ -6,4 +6,8 @@ router.get('/', (req, res) => {
     res.render('step-4-activity-homepage', { title: 'Step 4: Visioning Your Future' });
 });
 
+router.get('/a1', (req, res) => {
+    res.render('step-4-activity-1', { title: 'What to Visualize | Coolkit' });
+});
+
 module.exports = router;
