1c40bc36471272e6e272faccdde0dca015812247
tiffanywu0313 <hsinyunwu0313@gmail.com>
2019-06-21 14:44:36 -0700

    activity one and two placeholders

--- routes/step-1.js
+++ routes/step-1.js
@@ -10,4 +10,17 @@ router.get('/a1', (req, res) => {
     res.render('step-1-activity-1', { title: 'Story Collection | Coolkit' });
 });
 
+router.get('/a2', (req, res) => {
+    res.render('step-1-activity-2', { title: 'Photo Gallery | Coolkit' });
+});
+
+router.get('/a3', (req, res) => {
+    res.render('step-1-activity-3', { title: 'Photo Quiz | Coolkit' });
+});
+
+router.get('/a4', (req, res) => {
+    res.render('step-1-activity-4', { title: 'Non-Trivia QUiz | Coolkit' });
+});
+
+
 module.exports = router;
--- routes/step-2.js
+++ routes/step-2.js
@@ -10,8 +10,20 @@ router.get('/a1', (req, res) => {
     res.render('step-2-activity-1', { title: 'Urban Forest Quest | Coolkit' });
 });
 
+router.get('/a2', (req, res) => {
+    res.render('step-2-activity-2', { title: 'Climate Change Detective Quest | Coolkit' });
+});
+
+router.get('/a3', (req, res) => {
+    res.render('step-2-activity-3', { title: 'Carbon Visual Quest | Coolkit' });
+});
+
 router.get('/a4', (req, res) => {
     res.render('step-2-activity-4', { title: 'Habitat Mapping | Coolkit' });
+});
+
+router.get('/a5', (req, res) => {
+    res.render('step-2-activity-5', { title: 'Vulnerability Mapping | Coolkit' });
 });
 
 module.exports = router;
